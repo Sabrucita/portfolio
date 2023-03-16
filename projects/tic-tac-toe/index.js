9d4730aa4fe04e98bb9b24035b71e6d6ac23f86d
Sabrina <pereira.tsabrina@gmail.com>
2023-03-16 00:00:01 -0300

    TTT: finish game modal and styles added

--- projects/tic-tac-toe/index.js
+++ projects/tic-tac-toe/index.js
@@ -1,19 +1,31 @@
-const squares = document.querySelectorAll('.square')
+const squares = Array.from(document.querySelectorAll('.square'))
 const x = 'X';
 const o = 'O';
-let gameStatus = 'P1'
+let gameStatus = 'P1';
+const modal = document.querySelector('dialog')
+const modalText = modal.querySelector('h2')
+
 
 
 squares.forEach((square) => {
   square.addEventListener('click', () => {
+    if(gameStatus === 'PAUSE') return
+    if(square.textContent !== '') return
     square.innerText = gameStatus == 'P1' ? x : o;
+    const winningPosition = checkWinnerExistence()
+    if(typeof winningPosition === 'object'){
+      winner(winningPosition);
+      return
+    }
+    if(winningPosition === 'tie'){
+      showModal('Tie')
+    }
     gameStatus = gameStatus === 'P1' ? 'P2' : 'P1';
-    checkWinnerExistence()
   })
 });
 
 function checkWinnerExistence() {
-  const board = Array.from(squares).map(square => square.textContent)
+  const board = squares.map(square => square.textContent)
   console.log(board)
 
   //revisar horizontales
@@ -21,7 +33,7 @@ function checkWinnerExistence() {
     if(board[i] &&
     board[i] == board[i+1] &&
     board[i] == board[i+2]){
-      return winner([i,i+1,i+2])
+      return [i,i+1,i+2]
     }
   }
   //revisar verticales
@@ -29,22 +41,44 @@ function checkWinnerExistence() {
     if(board[i] &&
     board[i] == board[i+3] &&
     board[i] == board[i+6]){
-      return winner([i,i+3,i+6])
+      return [i,i+3,i+6]
     }
   }
   //revisar oblicuas
   if(board[0] &&
   board[0] === board[4] &&
   board[0] === board[8]){
-    return winner([0,4,8])
+    return [0,4,8]
   }
   if(board[2] &&
   board[2] === board[4] &&
   board[2] === board[6]){
-    return winner([0,4,6])
+    return [2,4,6]
   }
+
+  if(board.includes('')) return false;
+  return 'tie';
 }
 
 function winner(winningPosition) {
   console.log('ganador', winningPosition)
+  winningPosition.forEach(position => {
+    squares[position].classList.toggle('winner',true)
+  })
+  showModal('The winner is: ' + gameStatus)
+  gameStatus == 'PAUSE'
 }
+
+function showModal(text) {
+  modalText.innerText = text
+  modal.showModal()
+}
+
+modal.querySelector('button').addEventListener('click', () => {
+  squares.forEach(square => {
+    square.textContent= ''
+    square.classList.toggle('winner',false)
+    modal.close()
+    gameStatus = 'P1'
+  });
+})
